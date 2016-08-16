460f6f155ab64929ec2685fd00e4d067d9700f3a
xingjiepan <xingjiepan@gmail.com>
2016-08-15 19:30:52 -0700

    Optimize the code to make it run faster (and uglier)

--- manifolds/connectivity.py
+++ manifolds/connectivity.py
@@ -1,48 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_connectivity(distance_matrix, cutoff_radius):
+def get_connectivity(distance_set, cutoff_radius, num_points, connected_sets=None):
   '''Get the connectivity between points. Two points are
      thought to be connected if the distance between them
      are smaller than cutoff_radius. Return a list of sets
-     where each set contains some connected points.
+     which are the sets each point belongs to.
+     NOTE: the connected_sets argument will be MODIFIED!!!
   '''
-  num_points = distance_matrix.shape[0]
-  
-  # Set all points to be disconnected
-  
-  connected_sets = [ {i} for i in range(num_points) ]
+  # If no pre-initialized connect_sets is given, set all points to be disconnected
+ 
+  if connected_sets == None: 
+    connected_sets = [ {i} for i in range(num_points) ]
 
   # Merge two sets if they are connected
   
-  for i in range(num_points):
-    for j in range(i+1, num_points):
-      
-      if distance_matrix[i][j] < cutoff_radius:
+  for distance in distance_set:
+    i = distance[0]
+    j = distance[1]
+          
+    if distance[2] < cutoff_radius:
 
-        # Merge two sets
+      # Merge two sets
 
-        if connected_sets[i] == connected_sets[j]:
-          continue
+      if connected_sets[i] == connected_sets[j]:
+        continue
 
-        merged_set = connected_sets[i] | connected_sets[j]
-        for k in merged_set:
-          connected_sets[k] = merged_set
+      merged_set = connected_sets[i] | connected_sets[j]
+      for k in merged_set:
+        connected_sets[k] = merged_set
 
-  # Wrap the connected point sets into a list
-   
-  return_list = []
+  return connected_sets
+
+
+def wrap_connected_sets(connected_sets):
+  '''Wrap the connected point sets into clusters'''
+  cluster_list = []
    
   for s in connected_sets:
-    if not s in return_list:
-      return_list.append(s)
+    if not s in cluster_list:
+      cluster_list.append(s)
 
-  return return_list
+  return cluster_list
 
 
 def get_num_connected_manifolds(distance_matrix, cutoff_list):
   '''Get the number of connected manifolds for a list of cutoffs.'''
-  return [ len(get_connectivity(distance_matrix, cutoff)) for cutoff in cutoff_list ]
+  num_list = []
+  connected_sets = None
+
+  # Initialize the distance set
+  distance_set = set()
+  num_points = distance_matrix.shape[0]
+  
+  for i in range(num_points):
+    for j in range(i+1, num_points):
+      distance_set.add( (i, j, distance_matrix[i][j]) )
+  
+  for cutoff in cutoff_list:
+    connected_sets = get_connectivity(distance_set, cutoff, num_points, connected_sets)
+    num_list.append(len(wrap_connected_sets(connected_sets)))
+
+    # Remove all the distances that is smaller than the cutoff
+    # Because point pairs within those distances are already connected
+    for distance in distance_set.copy():
+      if distance[2] < cutoff:
+        distance_set.remove(distance)
+
+  return num_list
 
 
 def plot_num_connected_manifolds_vs_cutoffs(distance_matrix, average_cutoff_list, dimension):
@@ -59,9 +84,27 @@ def plot_num_connected_manifolds_vs_cutoffs(distance_matrix, average_cutoff_list
 def get_size_max_cluster(distance_matrix, cutoff_list):
   '''Get the max size of clusters for a list of cutoffs.'''
   max_sizes = []
+  
+  # Initialize the distance set
+  distance_set = set()
+  num_points = distance_matrix.shape[0]
+  
+  for i in range(num_points):
+    for j in range(i+1, num_points):
+      distance_set.add( (i, j, distance_matrix[i][j]) )
+ 
+  connected_sets = None 
   for cutoff in cutoff_list:
-    sizes = [ len(connected_sets) for connected_sets in get_connectivity(distance_matrix, cutoff) ]
+    connected_sets = get_connectivity(distance_set, cutoff, num_points, connected_sets)
+    sizes = [ len(connected_set) for connected_set in connected_sets ]
     max_sizes.append(max(sizes))
+    
+    # Remove all the distances that is smaller than the cutoff
+    # Because point pairs within those distances are already connected
+    for distance in distance_set.copy():
+      if distance[2] < cutoff:
+        distance_set.remove(distance)
+
   return max_sizes
 
 

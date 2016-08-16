fc188674f3b883ed58f12c4c35669b5e6f8057ba
xingjiepan <xingjiepan@gmail.com>
2016-08-15 18:25:08 -0700

    Update misc

--- run_analysis.py
+++ run_analysis.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
 
 import os
+import pickle
 
 from points_loaders.LoopLoader import LoopTorsionLoader
 from metrics.distance_calculators import TorusDistance
 from metrics.distance_matrix import calc_distance_matrix
+from manifolds.connectivity import plot_num_connected_manifolds_vs_cutoffs
+from manifolds.connectivity import plot_size_max_cluter 
 
-pdb_dir = 'inputs/test/30_KIC_with_fragments_1arb_RolandFragsInputsSettings_ideal'
-pdb_list = [ os.path.join(pdb_dir, f) for f in os.listdir(pdb_dir) ]
-
-points = LoopTorsionLoader.load_from_pdbs(pdb_list[0:20], 182, 193)
-#print(points)
+#pdb_dir = 'inputs/test/30_KIC_with_fragments_1arb_RolandFragsInputsSettings_ideal'
+#pdb_list = [ os.path.join(pdb_dir, f) for f in os.listdir(pdb_dir) ]
+#
+#points = LoopTorsionLoader.load_from_pdbs(pdb_list, 182, 193)
+##print(points)
 
 td_calc = TorusDistance(360)
-distance_matrix = calc_distance_matrix(points, td_calc)
-print(distance_matrix) 
+#max_distance = td_calc.max_distance(points[0].shape[0])
+max_distance = td_calc.max_distance(24)
+##print(max_distance)
+
+#distance_matrix = calc_distance_matrix(points, td_calc)
+##print(distance_matrix)
+
+#with open('test_dist_matrix.pickle', 'wb') as f:
+#  pickle.dump( distance_matrix, f, pickle.HIGHEST_PROTOCOL )
+
+distance_matrix = None
+with open('test_dist_matrix.pickle', 'rb') as f:
+  distance_matrix = pickle.load(f)
+
+cutoff_list = [ max_distance * i / 300 for i in range(100) ]
+#plot_num_connected_manifolds_vs_cutoffs(distance_matrix, cutoff_list) 
+plot_size_max_cluter(distance_matrix, cutoff_list) 
